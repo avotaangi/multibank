7c1d4673a05fab7c7137be8e9472a86ce6cb7fd2
avotaangi <alinaignatova67@gmail.com>
2025-09-22 00:16:17 +0300

    feat: Центрировать весь блок карт на экране
    - Контейнер карт теперь использует flex justify-center для центрирования
    - Ширина контейнера карт установлена в 400px
    - T-Bank карта: смещение уменьшено с +30px до +25px
    - Alpha Bank карта: смещение уменьшено с +60px до +50px
    - Весь блок карт теперь отображается по центру экрана
    - Улучшена визуальная симметрия и баланс

--- client/src/components/BankCardStack.jsx
+++ client/src/components/BankCardStack.jsx
@@ -104,10 +104,10 @@ const BankCardStack = () => {
 
 
   return (
-    <div className="relative w-full max-w-md mx-auto pb-8">
+    <div className="relative w-full flex justify-center pb-8">
       {/* Bank Cards Stack - Horizontal */}
       <div 
-        className="relative h-[220px] cursor-pointer select-none overflow-hidden"
+        className="relative h-[220px] w-[400px] cursor-pointer select-none overflow-hidden"
         onTouchStart={handleStart}
         onTouchMove={handleMove}
         onTouchEnd={handleEnd}
@@ -136,7 +136,7 @@ const BankCardStack = () => {
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[320px] h-[220px] rounded-[27px] z-20 transition-transform duration-200 hover:scale-105"
           style={{ 
             backgroundColor: cards[1].color,
-            transform: `translateX(calc(-50% + 30px - ${swipeOffset > 100 ? swipeOffset * 0.2 : 0}px))`
+            transform: `translateX(calc(-50% + 25px - ${swipeOffset > 100 ? swipeOffset * 0.2 : 0}px))`
           }}
         >
           <div className="p-6 h-full flex items-center justify-between">
@@ -155,7 +155,7 @@ const BankCardStack = () => {
           className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[320px] h-[220px] rounded-[27px] z-10 transition-transform duration-200 hover:scale-105"
           style={{ 
             backgroundColor: cards[2].color,
-            transform: `translateX(calc(-50% + 60px - ${swipeOffset > 150 ? swipeOffset * 0.1 : 0}px))`
+            transform: `translateX(calc(-50% + 50px - ${swipeOffset > 150 ? swipeOffset * 0.1 : 0}px))`
           }}
         >
           <div className="p-6 h-full flex flex-col justify-between">
